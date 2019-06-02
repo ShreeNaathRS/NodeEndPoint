81882caf3dc2f38af5f614b5b7672be4621a3ba1
ShreeNaathRS <rsshreenaath.com>
2019-06-03 01:00:43 +0530

    console origin and url change

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const cors = require('cors');
 
 const app = express();
 
-var allowedOrigins = ['https://dbsampleangular.herokuapp.com/',
-                      'https://dbsamplenode.herokuapp.com/'];
+var allowedOrigins = ['https://dbsampleangular.herokuapp.com',
+                      'https://dbsamplenode.herokuapp.com'];
 app.use(cors({
   origin: function(origin, callback){
     if(!origin) return callback(null, true);
     if(allowedOrigins.indexOf(origin) === -1){
       var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
+                'allow access from the specified Origin. Origin : '+origin;
       return callback(new Error(msg), false);
     }
     return callback(null, true);
